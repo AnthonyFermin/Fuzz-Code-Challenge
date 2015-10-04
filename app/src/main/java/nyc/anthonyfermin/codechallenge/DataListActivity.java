9b31d6e35d32652cb42251d0d7335718629310e2
AnthonyFermin <AnthonyFerminAFC@gmail.com>
2015-10-03 22:03:02 -0400

    initial commit

---
+++ app/src/main/java/nyc/anthonyfermin/codechallenge/DataListActivity.java
@@ -0,0 +1,15 @@
+package nyc.anthonyfermin.codechallenge;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by c4q-anthonyf on 10/3/15.
+ */
+public class DataListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
